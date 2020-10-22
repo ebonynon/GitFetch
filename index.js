1b0f5e5303716e5434590dcd4d54854692f012df
ebonynon <sithumsandeepap@gmail.com>
2020-10-22 14:38:50 +0530

    add: body-parser express.json()

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const git = require("./routes/api/git");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome to GitOps"));
 
