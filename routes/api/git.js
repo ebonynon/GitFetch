b789eec8f664cfacb9e65adecec08353d6a3fd53
ebonynon <sithumsandeepap@gmail.com>
2020-10-22 17:54:22 +0530

    setup logfile ⛳

--- routes/api/git.js
+++ routes/api/git.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const express = require("express");
 
 const dataPath = "./data/list.json";
+const logPath = "./data/log.json";
 const router = express.Router();
 
 router.post("/", (req, res) => {
@@ -9,7 +10,7 @@ router.post("/", (req, res) => {
     if (err) throw err;
 
     let _data = JSON.parse(data);
-
+    f(req);
     _data.push(req.body);
 
     fs.writeFile(dataPath, JSON.stringify(_data, null, 2), (err) => {
@@ -20,5 +21,26 @@ router.post("/", (req, res) => {
     });
   });
 });
+
+function f(req) {
+  fs.readFile(logPath, (_err, _data) => {
+    if (_err) throw err;
+
+    let logSet_ = JSON.parse(_data);
+
+    let q = {
+      repo: req.body.repo,
+      stime: req.body.time * 60000,
+      diff: [],
+    };
+
+    logSet_.push(q);
+
+    fs.writeFileSync(logPath, JSON.stringify(logSet_, null, 2), (werr) => {
+      if (werr) throw werr;
+      console.log("logSet_ written to file");
+    });
+  });
+}
 
 module.exports = router;
